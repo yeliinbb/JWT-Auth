02abac1de4f979ebfb483f9b4ff66d62ab091255
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-09-05 00:43:44 +0900

    프로필 변경 로직 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,137 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect, useRef, useState } from 'react';
+import { queryKeys } from '../lib/constants/queryKeys';
+import { getUserInfo, updateProfile } from '../api/auth';
+import { UserInfo } from '../types/auth.types';
+import defaultImg from '../assets/default-profile.jpg';
+
 const MyPage = () => {
-    return <div>MyPage</div>;
+  const [nickname, setNickname] = useState('');
+  const [previewImg, setPreviewImg] = useState<string | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const imgRef = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
+
+  const { data: userInfo, isSuccess } = useQuery<UserInfo>({
+    queryKey: [queryKeys.users],
+    queryFn: getUserInfo
+  });
+
+  useEffect(() => {
+    if (userInfo) {
+      setNickname(userInfo.nickname || '');
+      setPreviewImg(userInfo.avatar || null);
+    }
+  }, [userInfo]);
+
+  const profileUpdateMutation = useMutation<UserInfo, Error, FormData>({
+    mutationFn: updateProfile,
+    onSuccess: (updatedUserInfo) => {
+      queryClient.setQueryData(
+        [queryKeys.users, userInfo?.id],
+        updatedUserInfo
+      );
+      alert('프로필 업데이트가 완료되었습니다.');
+      setIsEditing(false);
+    },
+    onError: (error) => {
+      console.error('프로필 업데이트 실패:', error);
+      alert('프로필 업데이트에 실패했습니다. 다시 시도해주세요.');
+    }
+  });
+
+  const handleUpdateProfile = async () => {
+    const formData = new FormData();
+    formData.append('nickname', nickname);
+    const avatarFile = imgRef?.current?.files?.[0];
+    if (avatarFile) {
+      formData.append('avatar', avatarFile);
+    }
+    profileUpdateMutation.mutate(formData);
+  };
+
+  const onChangeImgFile = () => {
+    const file = imgRef?.current?.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      console.log('reader', reader);
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setPreviewImg(reader.result as string);
+      };
+    }
+  };
+
+  const handleClickEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditing(false);
+    setNickname(userInfo?.nickname || '');
+    setPreviewImg(userInfo?.avatar || null);
+  };
+
+  return (
+    <div>
+      <button>back</button>
+      {isSuccess && (
+        <div className="flex items-center">
+          <div className="flex flex-col">
+            <img
+              src={previewImg || userInfo.avatar || defaultImg}
+              alt="프로필 이미지"
+              className="w-32 h-32 object-cover rounded-full"
+            />
+            {isEditing && (
+              <>
+                <label htmlFor="profile-img" className="cursor-pointer">
+                  Upload Image
+                </label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  id="profile-img"
+                  ref={imgRef}
+                  onChange={onChangeImgFile}
+                  className="hidden"
+                />
+              </>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label>nickname</label>
+            <input
+              type="text"
+              id="nickname"
+              placeholder="변경할 닉네임을 입력해주세요."
+              defaultValue={userInfo ? userInfo?.nickname : nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              disabled={!isEditing}
+              className={
+                isEditing
+                  ? 'border border-gray-300'
+                  : 'border-none bg-transparent'
+              }
+            />
+          </div>
+        </div>
+      )}
+      {isEditing ? (
+        <div>
+          <button onClick={handleUpdateProfile}>수정</button>
+          <button onClick={handleCancelEdit}>취소</button>
+        </div>
+      ) : (
+        <button
+          className="bg-blue-400 font-medium text-white"
+          onClick={handleClickEdit}
+        >
+          프로필 수정하기
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default MyPage;
